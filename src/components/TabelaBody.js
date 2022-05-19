2e51695b39622f911c00dd61d78d38ed75b4f37b
Negildo Passos <negildojr@gmail.com>
2022-05-19 12:34:28 -0300

    Adicionando os Primeiros Componentes TabelaFoot, TabelaHeader, TabelaBody

---
+++ src/components/TabelaBody.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TabelaBody = () => (
+  
+   <tbody>
+       <tr>
+
+        <td>978-85-7522-632-2</td>
+        <td>CSS Grid Layout</td>
+        <td>Mauricio Samy</td>
+        <td>
+            <button className='botao remover'>Remover</button>
+        </td>
+       </tr>
+   </tbody>
+  )
+
+
+export default TabelaBody
---
+++ src/components/TabelaHead.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const  TabelaHead =() => (
+    <thead>
+        <tr>
+            <tr colSpan = '4'>Tabela de Livros</tr>
+        </tr>
+
+        <tr>
+
+           <th>ISBN</th>    
+           <th>Título</th>    
+           <th>Autor</th>    
+           <th></th> 
+
+        </tr>
+
+    </thead>
+)
+
+export default TabelaHead
